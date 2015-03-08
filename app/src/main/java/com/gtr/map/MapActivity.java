b00e623928b98b4af9657ec9c7d6f617442d366d
liuxiaoran <lxr_buaa_phd@buaa.edu.cn>
2015-03-08 10:46:32 +0800

    增加百度 map

---
+++ app/src/main/java/com/gtr/map/MapActivity.java
@@ -0,0 +1,41 @@
+package com.gtr.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.MapView;
+import com.gtr.R;
+
+/**
+ * Created by liuxiaoran on 15/3/8.
+ */
+public class MapActivity extends Activity {
+
+    MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.map_activity);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+}
